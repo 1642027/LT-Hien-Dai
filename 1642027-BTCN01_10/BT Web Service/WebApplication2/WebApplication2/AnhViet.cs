e8e3c33cc319b41a42f1a75a9eb20f8aff5d61f6
1642027 <chua.anime@gmail.com>
2017-10-20 13:01:41 +0900

    Final

---
+++ 1642027-BTCN01_10/BT Web Service/WebApplication2/WebApplication2/AnhViet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class AnhViet
+    {
+        private String tiengAnh;
+        private String tiengViet;
+        public string TiengAnh { get => tiengAnh; set => tiengAnh = value; }
+        public string TiengViet { get => tiengViet; set => tiengViet = value; }
+    }
+}
